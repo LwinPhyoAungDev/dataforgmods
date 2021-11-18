a893436a218f1b82ca409a6a50a8822d60c01202
LwinPhyoAungDev <94470026+LwinPhyoAungDev@users.noreply.github.com>
2021-11-18 18:11:06 +0630

    Update Sadatas.js

--- Sadatas.js
+++ Sadatas.js
@@ -4,7 +4,10 @@
         "url" : "https://fileservice.gtainside.com/downloads/ftpk/1530821184_Toyota Land Cruiser VXR 2017.zip",
         "photo url" : "https://drive.google.com/file/d/1-ZKj8IJnRVHueZwNrPwXYmhGQCZTRcan/view?usp=drivesdk",
         "user" : "true"
-    }    
+    },
+ 
+    {
+    }  
 
 ]
  
