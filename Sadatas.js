f0cb8fb45a119151915e31a266b0e58fff481c0a
LwinPhyoAungDev <94470026+LwinPhyoAungDev@users.noreply.github.com>
2021-11-19 11:07:11 +0630

    Update Sadatas.js

--- Sadatas.js
+++ Sadatas.js
@@ -4,7 +4,7 @@
         "Author" : "GARAgE m0D",
         "title" : "Land Cruiser",
         "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1598640894_Toyota Landcruiser.zip",
-        "photo_link" : "https://drive.google.com/file/d/1-b70YFJvYRANzxs-RFpYTFSa7eGiGjRY/view?usp=drivesdk",
+        "photo_link" : "http://drive.google.com/file/d/1-b70YFJvYRANzxs-RFpYTFSa7eGiGjRY/view?usp=drivesdk",
         "file_size" : "3.741 MB"
     },
  
@@ -12,7 +12,7 @@
         "Author" : "WESLEYBRGAMERS",
         "title" : "Hilux Revo",
         "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1593281504_2019 Toyota Hilux Revo.rar",
-        "photo_link" : "https://drive.google.com/file/d/1-cOHaywtCNKfac1TlOzmUr4rQQfTjBBP/view?usp=drivesdk",
+        "photo_link" : "http://drive.google.com/file/d/1-cOHaywtCNKfac1TlOzmUr4rQQfTjBBP/view?usp=drivesdk",
         "file_size" : "8.142 MB"
     }   
 
