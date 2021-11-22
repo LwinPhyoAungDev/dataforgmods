ac399eaaf7cda5ac6acec890986c2fb41eef81f8
LwinPhyoAungDev <94470026+LwinPhyoAungDev@users.noreply.github.com>
2021-11-22 20:06:06 +0630

    Update Sadatas.js

--- Sadatas.js
+++ Sadatas.js
@@ -14,7 +14,15 @@
         "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1593281504_2019 Toyota Hilux Revo.rar",
         "photo_link" : "https://lh3.googleusercontent.com/I8rtoo43NNtPnWrfgMG6ToCAHkQbkf1pHzV-PFyEnKSuzWYoCxAgSvixd7p5kNcE4wu8Kv1hzIqd7VExyb7d=w2695-h4532",
         "file_size" : "8.142 MB"
-    }   
+    },   
 
+    {
+        "author" : "spera3",
+        "title" : "Honda CRV 2011",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1593281504_2019 Toyota Hilux Revo.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/images/1333271059_gallery76.jpg",
+        "file_size" : "5.975 MB"
+    }
+    
 ]
  
