41255cce8c5c6745ac9e2dfc4f14b76650d8fc5f
LwinPhyoAungDev <94470026+LwinPhyoAungDev@users.noreply.github.com>
2021-11-22 20:45:56 +0630

    Update Sadatas.js

--- Sadatas.js
+++ Sadatas.js
@@ -19,17 +19,112 @@
     {
         "author" : "spera3",
         "title" : "Honda CRV 2011",
-        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1593281504_2019 Toyota Hilux Revo.rar",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpf/1333271059_honda crv 2011.rar",
         "photo_link" : "https://www.gtainside.com/downloads/images/1333271059_gallery76.jpg",
         "file_size" : "5.975 MB"
     },
     
-      {
+    {
         "author" : "jonny-b",
         "title" : "Honda CRV 1997",
         "download_link" : "https://fileservice.gtainside.com/downloads/ftpd/1204145847_Honda_CRV_97.rar",
         "photo_link" : "https://www.gtainside.com/downloads/download/Honda_CRV_97.JPG",
         "file_size" : "3.152 MB"
+    },
+    
+    {
+        "author" : "Ov Abhijeet",
+        "title" : "Honda Civic SI IVF",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1615666189_Honda civic SI IVF.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2021-03/thb_1615666189_gallery46.jpg",
+        "file_size" : "5.26 MB"
+    },
+    
+     {
+        "author" : "AR",
+        "title" : "2018 Honda Fit Facelift",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpk/1542828850_Honda Fit Facelift 2018.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2018-11/thb_1542828850_gta_sa%202018-11-21%2023-23-16-045.jpg",
+        "file_size" : "12.141 MB"
+    },
+    
+    {
+        "author" : "FHR artworks",
+        "title" : "2009 Honda Fit Rocket Bunny",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftph/1475795403_2009 Honda Fit Rocket Bunny.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2016-10/thb_1475795403_gta_sa%202016-10-06%2021-37-58-75.jpg",
+        "file_size" : "12.437 MB"
+    },
+    
+    {
+        "author" : "Reza A",
+        "title" : "2009 Honda Fit Stock",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftph/1403904332_Honda Fit 2009-Stock.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2014-06/thb_1403904332_Galery658.jpg",
+        "file_size" : "4.427 MB"
+    },
+    
+    {
+        "author" : "METEOR DESIGN",
+        "title" : "Bentley Mulsanne",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1607724193_mulsanne.zip",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2020-12/thb_1608463525_mta-screen_2020-12-20_16-23-44.jpg",
+        "file_size" : "13.442 MB"
+    },
+    
+    {
+        "author" : "Elite Customs, CaptainShakespeare",
+        "title" : "2018 Bentley Continental GT",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1591640714_Release GT W12.zip",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2020-06/thb_1591640714_GT_FE.jpg",
+        "file_size" : "2018 Bentley Continental GT",
+        "author_link" : "https://vk.com/siniestromods"
+    },
+    
+    {
+        "author" : "Roselia Design",
+        "title" : "2018 Bentley Continental GT3",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpk/1568189492_REL Bentley GT3.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2019-09/thb_1568189492_GTA_SA%202019-09-10%2019-50-39.jpg",
+        "file_size" : "1.254 MB",
+        "author_link" : "https://www.gtainside.com/en/sanandreas/cars/136844-2018-bentley-continental-gt3/"
+    },
+    
+    {
+        "author" : "crisscross543",
+        "title" : "BMW E36 M3 3.2",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1635573791_1635280119_BMW M3 E36 .zip",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2021-10/thb_1635280119_GTA_%20San%20Andreas%2026.10.2021%2019_44_08.jpg",
+        "file_size" : "12.549 MB",
+        "author_link" : "https://www.gtainside.com/en/sanandreas/cars/171124-bmw-e36-m3-3-2-m3-sound-mod-v-1-1-fix/"
+    },
+    
+    {
+        "author" : "MadoZZERX,spiderpawel,borasahin223",
+        "title" : "NFSMW 5-1-0 M3 GTR",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1634501794_NFSMW 510 M3 GTR.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2021-10/thb_1634501794_83.jpg",
+        "file_size" : "1.118 MB",
+        "author_link" : "https://www.gtainside.com/en/sanandreas/cars/170694-nfsmw-5-1-0-m3-gtr/"
+    },
+    
+    {
+        "author" : "The_Rz",
+        "title" : "BMW M5 E34",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1622056861_BMW M5 E34.rar",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2021-05/thb_1622056861_Screenshot1.jpg",
+        "file_size" : "21.895 MB",
+        "author_link" : "rezai5402@gmail.com"
+    },
+    
+    {
+        "author" : "jackergarden",
+        "title" : "BMW E36 M3 Coupe Shift",
+        "download_link" : "https://fileservice.gtainside.com/downloads/ftpl/1618507047_BMW_E36_M3_Coupe_Shift.zip
+",
+        "photo_link" : "https://www.gtainside.com/downloads/picr/2021-04/thb_1618507047_mta-screen_2021-04-13_22-59-25.jpg",
+        "file_size" : "3.181 MB",
+        "author_link" : "https://pskmtaarg.blogspot.com/"
     }
     
 ]
